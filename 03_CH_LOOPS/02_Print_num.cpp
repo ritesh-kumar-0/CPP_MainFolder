1dafde5481c0c46f0c305208a45a483e6d11e31e
Ritesh kumar <sidharthrk7890@gmail.com>
2026-03-03 15:04:32 +0530

    Loops questions

---
+++ 03_CH_LOOPS/02_Print_num.cpp
@@ -0,0 +1,12 @@
+//Print number from 1 to 5.
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    for(int i = 1; i <= 5; i++) {
+        cout << i << " ";
+    }
+
+    return 0;  
+} 
---
+++ 03_CH_LOOPS/03_Reverse_counting.cpp
@@ -0,0 +1,12 @@
+// Reverse Counting
+ 
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i = 5; i >= 1; i--) {
+    cout << i << " ";
+}
+return 0;
+
+}
