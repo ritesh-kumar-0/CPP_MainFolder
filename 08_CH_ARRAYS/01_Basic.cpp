b3dd00dddf0c67cd2fa0db7bc4c7f994d3caa021
Ritesh kumar <sidharthrk7890@gmail.com>
2026-05-05 14:58:31 +0530

    Array

--- 08_CH_ARRAYS/01_Basic.cpp
+++ 08_CH_ARRAYS/01_Basic.cpp
@@ -1,1 +1,24 @@
 //ARRAYS
+#include<iostream>
+using namespace std;
+
+int main(){
+    // 5 integers -> 6 ,2,8,5,0
+    int arr[5]; //Array declaration
+    arr[0] = 6;  // set
+    arr[1] = 2;
+    arr[2] = 8;
+    arr[3] = 5;
+    arr[4] = 0;
+
+    for(int i=0; i<=4; i++){
+        cout<<arr[i]<<" ";
+    }
+
+   // cout<<arr[0]<<" ";
+   // cout<<arr[1]<<" ";
+   // cout<<arr[2]<<" ";
+   // cout<<arr[3]<<" ";
+   // cout<<arr[4]<<" ";
+
+}
